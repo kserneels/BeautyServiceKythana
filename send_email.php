319eaaff8cd8f5fc4f1b44564cb550fcbe030975
r0897003 <r0897003@student.thomasmore.be>
2023-11-18 02:44:54 +0100

    small design changes

--- send_email.php
+++ send_email.php
@@ -1,20 +1,32 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields
     $voornaam = $_POST['voornaam'];
     $achternaam = $_POST['achternaam'];
     $email = $_POST['email'];
     $telefoonnummer = $_POST['telefoonnummer'];
     $bericht = $_POST['bericht'];
 
-    $to = "beautyservicekythana@hotmail.com";
+    // Email information
+    $recipient = "beautyservicekythana@hotmail.com";
     $subject = "Contact Form Submission";
-    $message = "Voornaam: $voornaam\nAchternaam: $achternaam\nEmail: $email\nTelefoonnummer: $telefoonnummer\nBericht: $bericht";
+    $email_content = "Voornaam: $voornaam\n";
+    $email_content .= "Achternaam: $achternaam\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Telefoonnummer: $telefoonnummer\n";
+    $email_content .= "Bericht:\n$bericht\n";
 
-    $headers = "From: $email";
+    // Email headers
+    $headers = "From: $voornaam $achternaam <$email>";
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully!";
+    // Send email
+    if (mail($recipient, $subject, $email_content, $headers)) {
+        // Email sent successfully, redirect back to form page
+        header("Location: your_form_page.php");
+        exit();
     } else {
-        echo "Oops! Something went wrong.";
+        echo "Oops! Something went wrong and we couldn't send your message.";
     }
+} else {
+    echo "There was an error with your submission. Please try again.";
 }
