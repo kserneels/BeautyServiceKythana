75e35dfb77432bd40b990fd64ab8d0428bbc90f4
kenzo serneels <r0889622@student.thomasmore.be>
2023-11-18 19:09:26 +0100

    send email

--- send_email.php
+++ send_email.php
@@ -1,32 +1,48 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields
+    // Retrieve form data
     $voornaam = $_POST['voornaam'];
     $achternaam = $_POST['achternaam'];
     $email = $_POST['email'];
     $telefoonnummer = $_POST['telefoonnummer'];
     $bericht = $_POST['bericht'];
 
-    // Email information
-    $recipient = "beautyservicekythana@hotmail.com";
-    $subject = "Contact Form Submission";
-    $email_content = "Voornaam: $voornaam\n";
-    $email_content .= "Achternaam: $achternaam\n";
-    $email_content .= "Email: $email\n";
-    $email_content .= "Telefoonnummer: $telefoonnummer\n";
-    $email_content .= "Bericht:\n$bericht\n";
+    // PHPMailer configuration
+    $mail = new PHPMailer(true);
 
-    // Email headers
-    $headers = "From: $voornaam $achternaam <$email>";
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'beautyservicekythana@gmail.com';                     //SMTP username
+    $mail->Password   = 'sydy xupe clgn mhwo';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    // Send email
-    if (mail($recipient, $subject, $email_content, $headers)) {
-        // Email sent successfully, redirect back to form page
-        header("Location: your_form_page.php");
-        exit();
-    } else {
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }
-} else {
-    echo "There was an error with your submission. Please try again.";
+    //Recipients
+    $mail->setFrom('beautyservicekythana@gmail.com', 'gmail');
+    $mail->addAddress('beautyservicekythana@hotmail.com', 'hotmail');     //Add a recipient
+    $mail->addReplyTo('info@example.com', 'Information');
+   
+
+     // Content
+     $mail->isHTML(true);
+     $mail->Subject = 'New Contact Form Submission';
+     $mail->Body    = "Voornaam: $voornaam <br> Achternaam: $achternaam <br> Email: $email <br> Telefoonnummer: $telefoonnummer <br> Bericht: $bericht";
+
+     $mail->send();
+     echo 'Message has been sent';
+ } catch (Exception $e) {
+     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+ }
 }
